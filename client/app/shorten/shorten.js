116138db3f0c8ebc33787c33f2e535441fdbcdd6
realmike33 <realmike33@gmail.com>
2015-01-16 20:04:24 -0800

    yeah

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,7 +3,8 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
     $scope.link = {};
     $scope.addLink = function(){
-      Links.addLink($scope.link)
+      console.log('I am the link!! ', url);
+      Links.addLink(url)
       .then(function(data){
         return data.links;
       }).catch(function(err){
